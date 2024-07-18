94f117c81d264d983a0db04eaf3a3cca308f4b8f
JongYo-Park <kioasdy1@gmail.com>
2024-07-18 14:30:08 +0900

    아이템 포션 생성
    아이템 포션

--- MySecondGit/Program.cs
+++ MySecondGit/Program.cs
@@ -16,12 +16,13 @@
         struct Item
         {
             public string name;
+            public ItemType type;
         }
         static void Main(string[] args)
         {
             Player player = new Player() { name = "용사", level = 1 };
             Monster monster = new Monster() { name = "고블린", hp = 500 };
-
+            Item item = new Item() { name = "포션", type = ItemType.Usable };
         }
     }
 }
