f8edd15fa971e547b7476bf7249cf60df6b71ae7
JongYo-Park <kioasdy1@gmail.com>
2024-07-18 12:34:31 +0900

    add Player

--- MySecondGit/Program.cs
+++ MySecondGit/Program.cs
@@ -2,9 +2,25 @@
 {
     internal class Program
     {
+        struct Player
+        {   
+            public string name;
+            public int level;
+        }
+        struct Monster
+        {
+            public string name;
+            public int hp;
+        }
+
+        struct Item
+        {
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Player player = new Player() { name = "용사", level = 1 };
+
         }
     }
 }
